346dc557fb4b0fd95ea99ef058ccec1a1bad2bc8
nagapriya1247 <74950586+nagapriya1247@users.noreply.github.com>
2024-10-06 11:43:22 +0530

    Update Jfrog.sh

--- Jfrog.sh
+++ Jfrog.sh
@@ -1,6 +1,6 @@
 ##Install in Amazon Ubuntu
 sudo usermod -aG docker $USER
-docker pull docker.bintray.io/jfrog/artifactory-oss:latest
+docker pull releases-docker.jfrog.io/jfrog/artifactory-oss:7.55.10
 sudo mkdir -p /jfrog/artifactory
 sudo chown -R 1030 /jfrog/
-docker run --name artifactory -d -p 8081:8081 -p 8082:8082 -v /jfrog/artifactory:/var/opt/jfrog/artifactory docker.bintray.io/jfrog/artifactory-oss:latest
+docker run --name artifactory -d -p 8081:8081 -p 8082:8082 -v /jfrog/artifactory:/var/opt/jfrog/artifactory releases-docker.jfrog.io/jfrog/artifactory-oss:7.55.10
